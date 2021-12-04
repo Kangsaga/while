ff215b634d0f3028cc55eae0abf8f01b04a72de3
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-04 15:14:15 +0900

    Add files via upload

---
+++ Exam02스캐너반복문.java
@@ -0,0 +1,34 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class Exam02스캐너반복문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		
+		
+		while(num <10) {
+			System.out.print("정수입력 : ");
+			num = sc.nextInt();
+			
+		}
+			//항상 조건식 >> boolean 타입만 사용한다!
+		//while(true) {
+			
+			//System.out.print("정수입력 : ");
+			//num = sc.nextInt();
+			//if(num >10) {
+				//break; //해당하는 반복문을 종료!
+				//분기문!!
+			//}
+		}
+		//System.out.println("종료되었습니다");
+		
+		
+		
+	}
+
+//}
