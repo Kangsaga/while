0fcf63d84b9041de229efd635ede15a491d1a78a
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-05 13:32:41 +0900

    Add files via upload

---
+++ Exam01while사용수출력.java
@@ -0,0 +1,18 @@
+
+public class Exam01while사용수출력 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//while 를 사용하여 콘솔에 1부터 10까지 출력
+		int num = 1;
+		 while (num <=10) {
+			 System.out.print(num++ + ", ");
+		 }
+		
+		
+		
+		
+	}
+
+}
